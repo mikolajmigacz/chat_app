1abf90aca9b7bf3e66b5ddbd40f6f9208f66aa25
mikolajmigacz <mig.mikolaj@gmail.com>
2022-10-14 19:55:59 +0200

    login and register screen with authentication

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:chat_app/service/auth_service.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  AuthService authService = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text('LOGOUT'),
+          onPressed: () {
+            authService.signOut();
+          },
+        ),
+      ),
+    );
+  }
+}
